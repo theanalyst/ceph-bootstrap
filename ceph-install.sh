4f7420d1ee663d65c0e89f1a726b8b361b6cbc04
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2014-08-19 16:13:44 +0530

    remove all fancy package checking
    - remove set -e as this errors out if pkgs dont exist
    - ceph-deploy can install newer cephs so dont do much processing

--- ceph-install.sh
+++ ceph-install.sh
@@ -1,19 +1,11 @@
 # A very minimal ceph install script, using ceph-deploy
-set -e
 set -x
 
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' ceph-deploy |grep "install ok installed")
-if [ "" == "$PKG_OK" ]; then
-    wget -q -O- 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc' | sudo apt-key add -
-    echo deb http://ceph.com/packages/ceph-extras/debian $(lsb_release -sc) main | sudo tee /etc/apt/sources.list.d/ceph-extras.list
-    sudo apt-add-repository 'deb http://ceph.com/debian-firefly/ $(lsb_release -sc)  main'
-    sudo apt-get update
-    sudo apt-get --yes install ceph-deploy
-fi
+sudo apt-get install -y ceph-deploy
 
 HOST=$(hostname)
 ceph-deploy purge $HOST
-ceph-deploy new $HOST
+ceph-deploy new $HOST --release firefly
 
 cat <<EOF >> ceph.conf
 osd pool default size=2
