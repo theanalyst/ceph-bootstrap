11357c607ecab162994ff26ab188e3547836f0f9
Abhishek Lekshmanan <abhishek.lekshmanan@gmail.com>
2014-08-20 12:51:50 +0530

    make objstore work properly

--- objstore.sh
+++ objstore.sh
@@ -2,10 +2,17 @@
 # A hack, create an object store service
 set -x
 
-if keystone service-list | ! grep -Fq swift
+keystone service-list |  grep -Fq swift
+if [ $? ]
 then
-    keystone service-create --name swift --type object-store
-    service_id = $(keystone service-list | grep swift | cut -d "|" -f 2 | tr -d " ")
-    my_url = "http://`(hostname -f)`/swift/v1"
-    keystone endpoint-create --region RegionOne --service-id $service_id --publicurl $my_url --internalurl $my_url --adminurl $my_url
+    service_id=$(keystone service-list | grep swift | cut -d "|" -f 2 | tr -d " ")
+    for i in $service_id
+        do
+          keystone service-delete $i
+        done
 fi
+
+keystone service-create --name swift --type object-store
+service_id=$(keystone service-list | grep swift | cut -d "|" -f 2 | tr -d " ")
+my_url="http://`(hostname -f)`/swift/v1"
+keystone endpoint-create --region RegionOne --service-id $service_id --publicurl ${my_url} --internalurl $my_url --adminurl $my_url
